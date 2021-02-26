2828369b0c095fbe77bb65c0251a60cfdcf3cff3
allie <69830149+alliejang99@users.noreply.github.com>
2021-02-26 17:08:36 +0900

    2021.02.26

---
+++ index.js
@@ -0,0 +1,14 @@
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function hadleClick(){
+    title.classList.toggle(CLICKED_CLASS);
+}
+
+function init(){
+    title.addEventListener("click", hadleClick);
+}
+
+init();
+
