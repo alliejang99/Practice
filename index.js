3a60f1abc2e3982d4534515d0298825143ab2a47
allie <69830149+alliejang99@users.noreply.github.com>
2021-03-15 16:33:05 +0900

    2021.03.15

--- index.js
+++ index.js
@@ -11,4 +11,3 @@ function init() {
 }
 
 init();
-
--- todo.js
+++ todo.js
@@ -1,6 +1,6 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-    toDoInput = toDoForm.querySelector("input"),
-    toDoList = document.querySelector(".js-toDoList");
+      toDoInput = toDoForm.querySelector("input"),
+      toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
