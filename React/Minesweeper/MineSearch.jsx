ec5273a07cd51718284a015e06a8e48070f5b2e8
allie <69830149+alliejang99@users.noreply.github.com>
2021-03-22 12:49:18 +0900

    2021.03.22

---
+++ React/Minesweeper/MineSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MineSearch = () => {
+  return (
+    <div>
+      dfsdfsd
+    </div>
+  )
+}
+
+export default MineSearch
---
+++ React/Minesweeper/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+    name: 'minesearch-setting',
+    mode: 'development', // 실서비스: production
+    devtool: 'eval',
+    resolve: {
+      extensions: ['.js','.jsx']
+    },
+
+    entry: {
+      index: ['./client'],
+    }, // 입력
+
+    module: {
+      rules: [
+        {
+          test: /\.jsx?/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              'react-refresh/babel',
+              'react-hot-loader/babel'
+            ],
+          },
+        }
+      ], // 규칙
+    },
+
+    plugins: [
+      new RefreshWebpackPlugin()
+    ],
+
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'index.js',
+    }, // 출력
+
+    devServer: {
+      publicPath: '/dist/',
+      hot: true,
+    }
+};
